66b190e841f0b2ad54840e14bfe2e2c401e24fbe
Bill Gross <bill.gross@me.com>
2019-06-30 18:03:36 -0500

    developing basic structure

--- src/structs.jl
+++ src/structs.jl
@@ -88,10 +88,10 @@ length(a::FreqArray) = size(a.data,1)
 duration(v::FreqData) = length(v)*(1/v.freq)
 
 getindex(v::FreqVector,I::itype) = FreqVector(view(v.data,I),v.freq,name=v.name,code=v.code)
-getindex(a::FreqArray,I::Union{itype,Int}) = FreqVector(view(a.data,I,Colon()),a.freq,names=a.names,code=a.code)
+getindex(a::FreqArray,I::Union{itype,Int}) = FreqArray(view(a.data,I,Colon()),a.freq,names=a.names,code=a.code)
 
 getindex(v::FreqVector,I::StepRange) = FreqVector(view(v.data,I),v.freq/I.step,name=v.name,code=v.code)
-getindex(a::FreqArray,I::StepRange) = FreqVector(view(a.data,I,Colon()),a.freq/I.step,names=a.names,code=a.code)
+getindex(a::FreqArray,I::StepRange) = FreqArray(view(a.data,I,Colon()),a.freq/I.step,names=a.names,code=a.code)
 
 getindex(v::FreqVector,I::Int) = v.data[I]
 
